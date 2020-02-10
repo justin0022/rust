d2495c74551aff132358f0e2b71af0b3f101f1be
Justin Lee <justinlee0022@gmail.com>
2020-02-10 11:42:40 -0800

    Added first cargo project

---
+++ hello_cargo/target/debug/deps/hello_cargo-26868795624fc898.d
@@ -0,0 +1,5 @@
+/Users/justin/Documents/rust/hello_cargo/target/debug/deps/hello_cargo-26868795624fc898.rmeta: src/main.rs
+
+/Users/justin/Documents/rust/hello_cargo/target/debug/deps/hello_cargo-26868795624fc898.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/justin/Documents/rust/hello_cargo/target/debug/hello_cargo: /Users/justin/Documents/rust/hello_cargo/src/main.rs
