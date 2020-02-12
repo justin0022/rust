509eb4d92e4d556d1fd9b15c0a1a5ba696c6b87b
Justin Lee <justinlee0022@gmail.com>
2020-02-11 16:29:30 -0800

    Created largest function

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,19 @@
-fn five() -> i32 {
-    5
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
 }
 
 fn main() {
-    let x = five();
+    let list = vec![23, 53, -213, 546364, 14];
 
-    println!("The value of x is: {}", x);
+    let result = largest(&list);
+
+    println!("The largest number is {}", result);
 }
---
+++ functions/target/debug/deps/functions-3e20357e25eee601.d
@@ -0,0 +1,5 @@
+/Users/justin/Documents/rust/functions/target/debug/deps/functions-3e20357e25eee601: src/main.rs
+
+/Users/justin/Documents/rust/functions/target/debug/deps/functions-3e20357e25eee601.d: src/main.rs
+
+src/main.rs:
