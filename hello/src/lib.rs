ec2a2c84a3e30301127a15fd5b0a3121f2eb0801
Justin Lee <justinlee0022@gmail.com>
2020-02-13 12:02:16 -0800

    Working on multithreaded web server

---
+++ hello/src/lib.rs
@@ -0,0 +1,20 @@
+pub struct ThreadPool;
+
+impl ThreadPool {
+  /// Create a new ThreadPool.
+  ///
+  /// The size is the number of threads in the pool.
+  ///
+  /// # Panics
+  ///
+  /// The `new` function will panic if the size is zero.
+  pub fn new(size: usize) -> ThreadPool {
+    ThreadPool
+  }
+
+  pub fn execute<F>(&self, f: F)
+  where
+    F: FnOnce() + Send + 'static,
+  {
+  }
+}
